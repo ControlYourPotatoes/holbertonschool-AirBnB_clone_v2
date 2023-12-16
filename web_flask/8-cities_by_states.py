01bf0a92d594bc0de37a9ea01306f77956cd0a96
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-12-15 20:20:52 -0400

    8 cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-""" This module starts a Flask web application """""
-from flask import Flask
-from flask import render_template
+"""
+flask model
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-""" Flask class and render_template method"""""
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+storage.all()
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown_data(self):
+    """
+        refrech data
+    """
     storage.close()
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
+    """ return all citie in the db  """
     states = storage.all(State)
+
     return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
