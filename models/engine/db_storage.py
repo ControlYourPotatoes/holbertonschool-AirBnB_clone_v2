b25dfc775f48efcd9b87b98f8277d16fb9bfa860
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-12-15 20:28:00 -0400

    update db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""
-Handles the storage when the engine depends on a MySQL database
-"""
+"""New engine DBStorage"""
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
 from models.base_model import Base
+from models.user import User
 from models.state import State
 from models.city import City
-from models.user import User
 from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
 
-classes = {'State': State, 'City': City, 'User': User,
-           'Place': Place, 'Review': Review, 'Amenity': Amenity}
+classes = {"State": State, "City": City, "User": User,
+           "Place": Place, "Review": Review, "Amenity": Amenity}
 
 
 class DBStorage:
-    """SqlAlchemy Database Storage"""
+    """DBStorage class"""
     __engine = None
     __session = None
 
     def __init__(self):
+        """DBStorage engine constructor"""
         user = getenv("HBNB_MYSQL_USER")
         pwd = getenv("HBNB_MYSQL_PWD")
         host = getenv("HBNB_MYSQL_HOST")
@@ -48,19 +48,27 @@ class DBStorage:
         return new_dict
 
     def new(self, obj):
+        """new method"""
         self.__session.add(obj)
 
     def save(self):
+        """save method"""
         self.__session.commit()
 
     def delete(self, obj=None):
+        """delete method"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """ reload method """
+        """reload method"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """ calls remove()
+        """
+        self.__session.close()
